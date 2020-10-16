24231b174eca634d31d0b47b31f3e0dca8926aaa
Maycon Benito <mayki17321@gmail.com>
2020-10-16 13:24:22 -0300

    chore: eslint fix

--- src/components/SmallArtistItem/index.js
+++ src/components/SmallArtistItem/index.js
@@ -3,12 +3,7 @@ import { useTranslation } from 'react-i18next';
 
 import fallback from '../../assets/images/fallback.png';
 import Image from '../Image';
-import {
-  Container,
-  Info,
-  Name,
-  Followers,
-} from './styles';
+import { Container, Info, Name, Followers } from './styles';
 
 function SmallArtistItem({ data, style, onClick }) {
   const { t } = useTranslation();
@@ -27,13 +22,8 @@ function SmallArtistItem({ data, style, onClick }) {
         onClick={onClick}
       />
       <Info>
-        <Name onClick={onClick}>
-          {data.name}
-        </Name>
-        <Followers>{`${data.followers} ${t(
-          'commons.followers'
-        )}`}
-        </Followers>
+        <Name onClick={onClick}>{data.name}</Name>
+        <Followers>{`${data.followers} ${t('commons.followers')}`}</Followers>
       </Info>
     </Container>
   );
